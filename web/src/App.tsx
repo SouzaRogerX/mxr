445e21384fb03d0d945d6c42c446800191cb84ef
Rogerio Souza <xrogeriosouza@gmail.com>
2020-12-27 14:06:27 -0300

    Alteracao da mensagem principal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Magazine X'RogerS</h1>
+      <h1>Aqui voce encontra a loja da natura e magazine luiza</h1>
     </div>
   );
 }
