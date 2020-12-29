7c34e96a4e54109a3e08a20f229dccc1253f250e
Rogerio Souza <xrogeriosouza@gmail.com>
2020-12-29 18:18:54 +0000

    Teste Layout responsivo

---
+++ web/src/pages/homeResp.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Global from "../styles/global";
+
+import Home from "../pages/home/home";
+
+const lorem =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, sed iure blanditiis voluptatum nulla quidem minus quam tempora obcaecati necessitatibus inventore! Vitae totam quam pariatur facilis fugit maxime adipisci eaque.";
+
+const data = [
+  {
+    id: Math.random(),
+    title: "Box titulo 1",
+    text: lorem,
+    bgColor: "#D5CAFA"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 2",
+    text: lorem,
+    bgColor: "#EDA9A9"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 3",
+    text: lorem,
+    bgColor: "#F2EE8D"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 4",
+    text: lorem,
+    bgColor: "#9FEACD"
+  }
+];
+
+function homeResp() {
+  return (
+    <>
+      <Global />
+      <Home boxData={data} />
+    </>
+  );
+}
+
+export default homeResp;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './pages/landing';
 import home from './pages/home';
 
+import HomeResp from "./pages/homeResp";
 
 function Routes() {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/land" component={Landing} />
-      <Route path="/" component={home} />
+      <Switch>
+        <Route path="/" component={HomeResp} />
+        <Route path="/land" component={Landing} />
+        <Route path="/home" component={home} />
       </Switch>
     </BrowserRouter>
   );
